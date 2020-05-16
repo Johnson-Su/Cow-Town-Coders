77ccf145bd7f0ea5472d52079d562a0e9833407d
Amy <amyh102@gmail.com>
2020-05-16 16:12:16 -0600

    linked firebase

---
+++ main.js
@@ -0,0 +1,23 @@
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+var firebase = require("firebase/app");
+
+// Add the Firebase products that you want to use
+require("firebase/auth");
+require("firebase/firestore");
+
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+import * as firebase from "firebase/app";
+
+// Add the Firebase services that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+
+// TODO: Replace the following with your app's Firebase project configuration
+var firebaseConfig = {
+    // ...
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);

bca2e80790b867eced0f1ca46491a099b2a3ccd4
Amy <amyh102@gmail.com>
2020-05-16 17:18:30 -0600

    module test

--- main.js
+++ main.js
@@ -1,23 +1,37 @@
-// Firebase App (the core Firebase SDK) is always required and
-// must be listed before other Firebase SDKs
-var firebase = require("firebase/app");
-
-// Add the Firebase products that you want to use
-require("firebase/auth");
-require("firebase/firestore");
-
-// Firebase App (the core Firebase SDK) is always required and
-// must be listed before other Firebase SDKs
 import * as firebase from "firebase/app";
 
 // Add the Firebase services that you want to use
 import "firebase/auth";
 import "firebase/firestore";
-
-// TODO: Replace the following with your app's Firebase project configuration
 var firebaseConfig = {
-    // ...
-  };
-  
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+    apiKey: "AIzaSyBSuD2Orc6lR-6eraS9zUDRoWKrdeP6W-g",
+    authDomain: "farmily-43dbd.firebaseapp.com",
+    databaseURL: "https://farmily-43dbd.firebaseio.com",
+    projectId: "farmily-43dbd",
+    storageBucket: "farmily-43dbd.appspot.com",
+    messagingSenderId: "135754925154",
+    appId: "1:135754925154:web:b47b1216d393172150fd3b"
+    };
+    
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+        function writeData(){
+            firebase.database().ref("Farmer").set({
+                email: document.getElementById("email").value,
+                farm: document.getElementById("farm").value,
+                password: document.getElementById("password").value,
+                confirm: document.getElementById("confirm").value
+            });
+
+            var firebaseRef = firebase.database().ref();
+                firebaseRef.child("Farmer").push({
+                    email: document.getElementById("email").value,
+                farm: document.getElementById("farm").value,
+                password: document.getElementById("password").value,
+                confirm: document.getElementById("confirm").value
+                }).then(function() {
+                    document.location.href = "new-farmer2.html";
+                });
+            // document.location.href = "new-farmer2.html";
+        }

7f56ec77810953193e150526f5bcfdefe1849f44
Amy <amyh102@gmail.com>
2020-05-16 19:34:48 -0600

    firebase write data finished

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-import * as firebase from "firebase/app";
-
-// Add the Firebase services that you want to use
-import "firebase/auth";
-import "firebase/firestore";
 var firebaseConfig = {
     apiKey: "AIzaSyBSuD2Orc6lR-6eraS9zUDRoWKrdeP6W-g",
     authDomain: "farmily-43dbd.firebaseapp.com",
@@ -11,50 +6,52 @@ var firebaseConfig = {
     storageBucket: "farmily-43dbd.appspot.com",
     messagingSenderId: "135754925154",
     appId: "1:135754925154:web:b47b1216d393172150fd3b"
-    };
+};
 
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+   
+function writeData1(){
+    farmerRef = firebase.database().ref();
+    newFarmerRef = farmerRef.push();
+    farmerID = newFarmerRef.key;
+    // document.cookie = farmerRef.toString();
+    // document.location.href = "new-farmer2.html";
+    // console.log("in writedata1: " + document.cookie);
+    firebase.database().ref("Farmer/" + farmerID).set({
+        email: document.getElementById("email").value,
+        farm: document.getElementById("farm").value,
+        password: document.getElementById("password").value,
+        confirm: document.getElementById("confirm").value
+    });
 
-        function writeData1(){
-            var newFarmerRef = farmerRef.push();
-            var farmerID = newFarmerRef.key;
+    farmerRef.child("Farmer/" + farmerID).push({
+        email: document.getElementById("email").value,
+        farm: document.getElementById("farm").value,
+        password: document.getElementById("password").value,
+        confirm: document.getElementById("confirm").value
+    }).then(function() {
+        document.location.href = "new-farmer2.html";
+    });
+}
 
-            firebase.database().ref("Farmer/" + farmerID).set({
-                email: document.getElementById("email").value,
-                farm: document.getElementById("farm").value,
-                password: document.getElementById("password").value,
-                confirm: document.getElementById("confirm").value
-            });
+function writeData2(){
+    // console.log(farmerRef)
+    console.log(document.cookie);
+    // firebase.database().ref("Farmer/" + farmerID + "/New Product").set({
+    //     Product: document.getElementById("product").value,
+    //     Units: document.getElementById("units").value,
+    //     Quantity: document.getElementById("quantity").value,
+    //     Price: document.getElementById("price").value
+    // });
 
-            var firebaseRef = firebase.database().ref();
-                firebaseRef.child("Farmer/" + farmerID).push({
-                    email: document.getElementById("email").value,
-                    farm: document.getElementById("farm").value,
-                    password: document.getElementById("password").value,
-                    confirm: document.getElementById("confirm").value
-                }).then(function() {
-                    document.location.href = "new-farmer2.html";
-                });
-        }
+    // farmerRef.child("Sell").push({
+    //     Product: document.getElementById("product").value,
+    //     Units: document.getElementById("units").value,
+    //     Quantity: document.getElementById("quantity").value,
+    //     Price: document.getElementById("price").value
+    // }).then(function() {
+    //     // document.location.href = "__PLACEHOLDER___";
+    // });
 
-
-        function writeData2(){
-              firebase.database().ref("Sell").set({
-                  Product: document.getElementById("product").value,
-                  Units: document.getElementById("units").value,
-                  Quantity: document.getElementById("quantity").value,
-                  Price: document.getElementById("price").value
-              });
-
-        var firebaseRef = firebase.database().ref();
-            firebaseRef.child("Sell").push({
-                Product: document.getElementById("product").value,
-                Units: document.getElementById("units").value,
-                Quantity: document.getElementById("quantity").value,
-                Price: document.getElementById("price").value
-            }).then(function() {
-                document.location.href = "__PLACEHOLDER___";
-            });
-
-        }
+}

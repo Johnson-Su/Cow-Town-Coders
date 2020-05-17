ae39752e51c8a4743708332b8fa02c5d29dc5b60
Amy <amyh102@gmail.com>
2020-05-16 18:04:15 -0600

    push data???

--- main.js
+++ main.js
@@ -17,20 +17,12 @@ var firebaseConfig = {
     firebase.initializeApp(firebaseConfig);
 
         function writeData1(){
-            // firebase.database().ref("Farmer").add({
+            // firebase.database().ref("Farmer").set({
             //     email: document.getElementById("email").value,
             //     farm: document.getElementById("farm").value,
             //     password: document.getElementById("password").value,
             //     confirm: document.getElementById("confirm").value
             // });
-            var database = firebase.database();
-            var newFarmer = postListRef.push();
-            newFarmer.set({
-                email: document.getElementById("email").value,
-                farm: document.getElementById("farm").value,
-                password: document.getElementById("password").value,
-                confirm: document.getElementById("confirm").value
-            });
 
             var firebaseRef = firebase.database().ref();
                 firebaseRef.child("Farmer").push({
